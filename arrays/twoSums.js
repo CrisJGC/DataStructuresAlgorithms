8b0b8fafff25ef63cc602a38e3d7980b4a767e82
Cristian Gallegos <jacob.gallegos.c@gmail.com>
2023-04-25 14:41:58 -0600

    more ways to solve it

--- arrays/twoSums.js
+++ arrays/twoSums.js
@@ -27,14 +27,35 @@ function twoSums1(nums, target) {
     }
   }
 }
-console.log(twoSums(nums, target))
+// console.log(twoSums(nums, target))
 
-function twoSums2(nums, target) {
-  let remaing = nums.map(num => target - num)
-  for ( let i = 0 ; i < nums.length; i++){
-    if( remaing === nums[i] ){
-      
+// function binarySearch(arr, target) {
+//   const complementMap = new Map();
+//   for (let i = 0; i < arr.length; i++) {
+//     const complement = target - arr[i];
+//     if (complementMap.has(complement)) {
+//       return [complementMap.get(complement), i];
+//     }
+//     complementMap.set(arr[i], i);
+//   }
+//   return null;
+// }
+
+function anotherBin(nums, target){
+  let left = 0
+  let right  = nums.length-1
+
+  while(left < right){
+    const complement = nums[left]+ nums[right]
+    if(complement === target) {
+      return [left, right];
+    } else if (complement < target) {
+      left++;
+    } else {
+      right--;
     }
-    
   }
+  return null;
 }
+
+console.log(anotherBin(nums, target))
